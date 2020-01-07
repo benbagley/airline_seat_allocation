9dca5dd0942f345ccc9fe18df66fd6c68eb80758
Ben Bagley <hello@benbagley.dev>
2020-01-07 13:46:35 +0000

    Added Inheritance and Implementation Sharing for all aircraft

--- airtravel.py
+++ airtravel.py
@@ -134,13 +134,19 @@ class Flight:
                     yield passenger, "{}{}".format(row, letter)
 
 
-class AirbusA319:
+class Aircraft:
     def __init__(self, registration):
         self._registration = registration
 
     def registration(self):
         return self._registration
 
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len(row_seats)
+
+
+class AirbusA319(Aircraft):
     def model(self):
         return "Airbus A319"
 
@@ -148,13 +154,7 @@ class AirbusA319:
         return range(1, 23), "ABCDEF"
 
 
-class Boeing777:
-    def __init__(self, registration):
-        self._registration = registration
-
-    def registration(self):
-        return self._registration
-
+class Boeing777(Aircraft):
     def model(self):
         return "Boeing 777"
 
